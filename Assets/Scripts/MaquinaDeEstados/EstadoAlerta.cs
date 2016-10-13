dbd197e43c72dc0e4a53e9d6af614d0831971045
Edu <info@did-web.com>
2016-10-13 14:55:59 +0200

    maquina de estados primeros pasos ia

---
+++ Assets/Scripts/MaquinaDeEstados/EstadoAlerta.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EstadoAlerta : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
